3585d38708a7140c0ad4cbae66e24a204845a59f
Rene Peinthor <peinthor@gmail.com>
2017-09-04 19:52:11 +0200

    add charset utf8 to html

--- gosgal.go
+++ gosgal.go
@@ -17,7 +17,7 @@ var html_head string = `<!DOCTYPE html>
 <html>
 <head>
   <title>/Pictures</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="viewport" charset="utf-8" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.2/photoswipe.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.2/default-skin/default-skin.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.2/photoswipe.min.js"></script>
